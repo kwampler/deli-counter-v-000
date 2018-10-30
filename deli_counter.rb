75267ade30583a7774fa2306bc0da391346c17af
katejsenger <katejsenger@yahoo.com>
2018-10-30 02:41:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(line, new_person)
 end
 def now_serving(line)
   if line.length == 0 
-    puts"There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{line[0]}.
     line.shift 
